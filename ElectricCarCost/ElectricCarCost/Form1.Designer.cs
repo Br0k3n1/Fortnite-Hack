c7a6c8f2058964157eb535b9642c8e89d27b6e24
Br0k3n1 <83535098+Br0k3n1@users.noreply.github.com>
2023-03-31 02:25:18 -0400

    Add files via upload

---
+++ ElectricCarCost/ElectricCarCost/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ElectricCarCost
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addressInput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.hoursInput = new System.Windows.Forms.NumericUpDown();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.billLabel = new System.Windows.Forms.Label();
+            this.hoursLabel = new System.Windows.Forms.Label();
+            this.addressLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.hoursInput)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(60, 12);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(208, 23);
+            this.nameInput.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address: ";
+            // 
+            // addressInput
+            // 
+            this.addressInput.Location = new System.Drawing.Point(73, 46);
+            this.addressInput.Name = "addressInput";
+            this.addressInput.Size = new System.Drawing.Size(195, 23);
+            this.addressInput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Charging Hours: ";
+            // 
+            // hoursInput
+            // 
+            this.hoursInput.Location = new System.Drawing.Point(115, 83);
+            this.hoursInput.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.hoursInput.Name = "hoursInput";
+            this.hoursInput.Size = new System.Drawing.Size(153, 23);
+            this.hoursInput.TabIndex = 6;
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(106, 128);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(97, 31);
+            this.calcButton.TabIndex = 7;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.calcButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.billLabel);
+            this.panel1.Controls.Add(this.hoursLabel);
+            this.panel1.Controls.Add(this.addressLabel);
+            this.panel1.Controls.Add(this.nameLabel);
+            this.panel1.Controls.Add(this.headerLabel);
+            this.panel1.Location = new System.Drawing.Point(23, 174);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 159);
+            this.panel1.TabIndex = 8;
+            // 
+            // billLabel
+            // 
+            this.billLabel.AutoSize = true;
+            this.billLabel.Location = new System.Drawing.Point(49, 120);
+            this.billLabel.Name = "billLabel";
+            this.billLabel.Size = new System.Drawing.Size(54, 15);
+            this.billLabel.TabIndex = 4;
+            this.billLabel.Text = "Final Bill:";
+            // 
+            // hoursLabel
+            // 
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Location = new System.Drawing.Point(49, 91);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(127, 15);
+            this.hoursLabel.TabIndex = 3;
+            this.hoursLabel.Text = "Hours Spent Charging:";
+            // 
+            // addressLabel
+            // 
+            this.addressLabel.AutoSize = true;
+            this.addressLabel.Location = new System.Drawing.Point(49, 64);
+            this.addressLabel.Name = "addressLabel";
+            this.addressLabel.Size = new System.Drawing.Size(55, 15);
+            this.addressLabel.TabIndex = 2;
+            this.addressLabel.Text = "Address: ";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(49, 38);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(45, 15);
+            this.nameLabel.TabIndex = 1;
+            this.nameLabel.Text = "Name: ";
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.headerLabel.Location = new System.Drawing.Point(0, 0);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(273, 28);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "Customer Bill Listing:";
+            this.headerLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(106, 345);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(97, 31);
+            this.clearButton.TabIndex = 9;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(320, 388);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.calcButton);
+            this.Controls.Add(this.hoursInput);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Charging Cost Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.hoursInput)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox nameInput;
+        private Label label1;
+        private Label label2;
+        private TextBox addressInput;
+        private Label label3;
+        private NumericUpDown hoursInput;
+        private Button calcButton;
+        private Panel panel1;
+        private Button clearButton;
+        private Label headerLabel;
+        private Label billLabel;
+        private Label hoursLabel;
+        private Label addressLabel;
+        private Label nameLabel;
+    }
+}
